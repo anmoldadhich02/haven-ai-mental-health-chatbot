aaa0b746938ad510ba54c5567aa26c3732b50daf
anmoldadhich02 <anmol.dadhich2023@vitstudent.ac.in>
2026-06-25 15:07:29 +0530

    just testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
+#Hello this is change for feature branch 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.routes import chat
 from app.routes import mood
 from app.routes import journal
